efeb2d6c1b804c28e6f09e0504cc3c8ecf198880
Gab705 <angeeloge8@gmail.com>
2025-03-11 12:10:08 +0000

    troisrepos

--- resources/views/groupeindex.blade.php
+++ resources/views/groupeindex.blade.php
@@ -99,7 +99,7 @@
 
     <div class="flex items-center space-x-4">
         <p class="text-blue-600">
-            {{ $transactionGroupes->montant }}
+            {{ $transactionGroupes->amount }}
         </p>
         <a href="{{ route('majentreegroupe', $transactionGroupes->id) }}">
                 <button type="submit" class="cursor-pointer">
