fd2f88b56f725f0126573008c949945eb093cbdd
Gab705 <angeeloge8@gmail.com>
2025-03-11 11:53:05 +0000

    troisrepos

--- resources/views/entreegroupe.blade.php
+++ resources/views/entreegroupe.blade.php
@@ -17,8 +17,8 @@
                 <input type="text" name="name" id="name" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="montant" class="block text-gray-700">Montant</label>
-                <input type="number" name="montant" id="montant" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="amount" class="block text-gray-700">Montant</label>
+                <input type="number" name="amount" id="amount" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <button type="submit" class="w-full text-white py-2 px-4 rounded-lg bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Ajouter</button>
         </form>
